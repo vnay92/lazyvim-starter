c9338cec5ddc120e32327dbc8a89fff0e588fb01
Vinay Bharadwaj <vinay.b@vananam.com>
2025-01-22 21:57:51 +0530

    Enabled Go Debugger

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,10 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
