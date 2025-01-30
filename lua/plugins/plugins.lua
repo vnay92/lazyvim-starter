4faa44e3eb3cfa237dff52963f9cadd98ec09e6d
Vinay Bharadwaj <vinay.b@vananam.com>
2025-01-30 17:57:24 +0530

    Added go.nvim

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -276,4 +276,19 @@ return {
       vim.api.nvim_set_keymap("n", "<Leader>dc", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true })
     end,
   },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }
